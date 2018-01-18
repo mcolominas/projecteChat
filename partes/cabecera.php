89a9c2759fd3cf93f23f66d3ed1a91c53e1c3bd5
Alejandro Novoa <anovoamontolio@iesesteveterradas.cat>
2018-01-18 19:51:05 +0100

    estilo

--- partes/cabecera.php
+++ partes/cabecera.php
@@ -1,4 +1,4 @@
-<div class="text-center" style="overflow: hidden;">
+<div id="cabecera" class="text-center" style="overflow: hidden;">
   <a href="<?php echo getURLAbsolute()."index.php" ?>"><img src="<?php echo getURLAbsolute(); ?>img/logo.jpg"></a>
   <img class="hidden-xs" src="<?php echo getURLAbsolute(); ?>img/banner_cabecera.jpg">
 </div>
