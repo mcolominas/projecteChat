4835a3dd9ed9c1e6eff7f6d2bd77e4885be6bda7
AWS2-05 <mcolominasrojas@iesesteveterradas.cat>
2018-01-24 17:55:06 +0100

    nuevos iconos

--- paginas/chatroom.php
+++ paginas/chatroom.php
@@ -6,8 +6,6 @@
   <?php require_once("../partes/head.php") ?>
   <link rel="stylesheet" type="text/css" href="<?php echo getURLAbsolute(); ?>css/chatroom.css">
   <script src="<?php echo getURLAbsolute(); ?>js/chatroom.js"></script>
-  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
-  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
 </head>
 <body>
 <?php require_once("../partes/cabecera.php") ?>
@@ -21,29 +19,6 @@
         <div class="well col-sm-10">
             <div id="frame">
                 <div id="sidepanel">
-                    <div id="profile">
-                        <div class="wrap">
-                            <img id="profile-img" src="http://emilcarlsson.se/assets/mikeross.png" class="online" alt="" />
-                            <p>Mike Ross</p>
-                            <i class="fa fa-chevron-down expand-button" aria-hidden="true"></i>
-                            <div id="status-options">
-                                <ul>
-                                    <li id="status-online" class="active"><span class="status-circle"></span> <p>Online</p></li>
-                                    <li id="status-away"><span class="status-circle"></span> <p>Away</p></li>
-                                    <li id="status-busy"><span class="status-circle"></span> <p>Busy</p></li>
-                                    <li id="status-offline"><span class="status-circle"></span> <p>Offline</p></li>
-                                </ul>
-                            </div>
-                            <div id="expanded">
-                                <label for="twitter"><i class="fa fa-facebook fa-fw" aria-hidden="true"></i></label>
-                                <input name="twitter" type="text" value="mikeross" />
-                                <label for="twitter"><i class="fa fa-twitter fa-fw" aria-hidden="true"></i></label>
-                                <input name="twitter" type="text" value="ross81" />
-                                <label for="twitter"><i class="fa fa-instagram fa-fw" aria-hidden="true"></i></label>
-                                <input name="twitter" type="text" value="mike.ross" />
-                            </div>
-                        </div>
-                    </div>
                     <div id="search">
                         <label for=""><i class="fa fa-search" aria-hidden="true"></i></label>
                         <input type="text" placeholder="Search contacts..." />
@@ -153,7 +128,7 @@
                         </ul>
                     </div>
                     <div id="bottom-bar">
-                        <button id="addcontact"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> <span>Add contact</span></button>
+                        <button id="addcontact"><i class="material-icons">group_add</i> <span>Nuevo Grupo</span></button>
                         <button id="settings"><i class="fa fa-cog fa-fw" aria-hidden="true"></i> <span>Settings</span></button>
                     </div>
                 </div>
@@ -205,8 +180,7 @@
                     </div>
                     <div class="message-input">
                         <div class="wrap">
-                        <input type="text" placeholder="Write your message..." />
-                        <i class="fa fa-paperclip attachment" aria-hidden="true"></i>
+                        <input type="text" placeholder="Escribe tu mensaje..." />
                         <button class="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
                         </div>
                     </div>
--- partes/head.php
+++ partes/head.php
@@ -6,6 +6,9 @@
 <link rel="stylesheet" type="text/css" href="<?php echo getURLAbsolute(); ?>css/main.css"/>
 <link rel="stylesheet" type="text/css" href="<?php echo getURLAbsolute(); ?>css/loginModal.css"/>
 <link rel="stylesheet" type="text/css" href="<?php echo getURLAbsolute(); ?>css/menu.css"/>
+<!-- iconos -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="<?php echo getURLAbsolute(); ?>bootstrap-3.3.7/js/bootstrap.min.js"></script>
